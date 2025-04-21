063b39c3793300e667fd33af74a895051dcb177f
Panchenko Denis <Pandenisko@gmail.com>
2025-04-22 00:27:17 +0300

    updated folder name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "modern-normalize";
-import App from "./compnents/App/App";
+import App from "./components/App/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
